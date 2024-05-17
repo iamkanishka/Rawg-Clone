e6adfa77bbaed08e95845cf24d09a27487d55cc3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-05-17 15:39:08 +0530

    Worked on Authentication with Login Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Route } from '@angular/router';
+import { AuthLayoutComponent } from 'src/core/layout/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from 'src/core/main-layout/main-layout.component';
 import { gameListRoutes } from 'src/routes/games-page/game-list.routes';
 
@@ -8,5 +9,20 @@ export const appRoutes: Route[] = [
         path:'',
         component:MainLayoutComponent,
         children:[...gameListRoutes]
-    }
+    },
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../routes/auth/auth.routes').then((r) => r.AUTH_ROUTES),
+          },
+        ],
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
 ];
